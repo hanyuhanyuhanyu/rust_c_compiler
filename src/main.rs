64fe668712e6eedef6920bf56890da3808a8a1b1
umano <concon.wolf@gmail.com>
2025-02-26 03:33:15 +0000

    加減算

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,59 @@
 use std::env;
-
+fn build_calc_lines(nums: Vec<i32>, opes: Vec<char>, space_count: usize) -> String {
+    // TODO細かいチェックはぬき
+    let mut lines: Vec<String> = Vec::new();
+    lines.push(format!("mov rax, {}", nums[0]).into());
+    for (i, ope) in opes.iter().enumerate() {
+        let instruction: String = match ope {
+            '+' => "add",
+            '-' => "sub",
+            _ => "",
+        }
+        .into();
+        lines.push(format!("{} rax, {}", instruction, nums[i + 1]).into());
+    }
+    return lines
+        .iter()
+        .map(|l| format!("{}{}", " ".repeat(space_count), l))
+        .collect::<Vec<String>>()
+        .join("\n");
+}
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         println!("no arg given");
         return;
     }
-    println!(".intel_syntax noprefix
+    let mut nums: Vec<i32> = Vec::new();
+    let mut opes: Vec<char> = Vec::new();
+    let mut buffer: i32 = 0;
+    for c in args[1].chars() {
+        if c.is_numeric() {
+            buffer *= 10;
+            buffer += c.to_digit(10).unwrap() as i32;
+            continue;
+        }
+        match c {
+            '+' | '-' => {
+                nums.push(buffer);
+                opes.push(c);
+            }
+            _ => {
+                println!("unexpected char: {}", c);
+                return;
+            }
+        }
+        buffer = 0;
+    }
+    nums.push(buffer);
+    println!(
+        ".intel_syntax noprefix
 .globl main
 main:
-        mov rax,{} 
-        ret
-    ", args[1]);
-
+{}
+    ret
+        ",
+        build_calc_lines(nums, opes, 4)
+    );
+    return;
 }
